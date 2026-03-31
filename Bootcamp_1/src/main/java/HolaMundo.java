4ebb513c9cada1a1a7b5a7dee9016badf9b0b0ee
Alejandro Varela Ortiz <AVARELAO@invex.com>
2026-03-31 17:09:43 -0600

    doc: Avance modulo 1 bootcamp

---
+++ Bootcamp_1/src/main/java/HolaMundo.java
@@ -0,0 +1,34 @@
+// Archivo: HolaMundo.java
+// La convención de nombres en Java: UpperCamelCase para clases
+//Codigo de Alejandro Varela Oritz
+
+
+public class HolaMundo {
+    // main: el punto de entrada de cualquier programa Java
+    // La JVM busca exactamente este método para iniciar la ejecución
+
+    /*
+     * Comentario Javadoc — genera documentación HTML automáticamente
+     * @param nombre El nombre a saludar
+     * @return El saludo formateado
+     * @author Tu Nombre
+     */
+
+    //metodo
+    public String saludar(String nombre){
+        return "Saludos a: " + nombre ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo del backend Java!");
+        HolaMundo holaMundo = new HolaMundo();
+        System.out.println(holaMundo.saludar("Alejandro Varela"));
+        System.out.println(HolaMundo.saludo("Alejandro"));
+    }
+
+    //metodo
+    public static String saludo(String nombre){
+        return "Saludos a: " + nombre ;
+    }
+
+}
